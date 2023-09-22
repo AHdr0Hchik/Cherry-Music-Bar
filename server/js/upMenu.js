cba5f3866a4f7fd58caa7b63f8808d01f936dbd2
AHdr0Hchik <xomenko.andrey2201@gmail.com>
2023-09-22 14:42:22 +0300

    v2.3

--- server/js/upMenu.js
+++ server/js/upMenu.js
@@ -1,9 +1,12 @@
 $(window).scroll(function(){
     const $el = $('.upMenu');
     const $el3 = $('#header');
+    const $el4 = $('.empty');
     let heightHeader = parseInt($el3.css( "height" ));
     let heightMenu = $el.css( "height" );
-
+    $el4.css({'height': heightMenu});
+    console.log(heightHeader);
+    console.log($(this).scrollTop)
     let isPositionFixed = ($el.css('position') == 'fixed');
     if ($(this).scrollTop() > heightHeader && !isPositionFixed){
         $el.css({'position': 'fixed', 'top': '0px', 'backdropFilter': 'blur(3px)'});
