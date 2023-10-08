1d3efef971011b140b7e49a483ae62f0eadfca52
AHdr0Hchik <xomenko.andrey2201@gmail.com>
2023-10-08 17:40:16 +0300

    cart is done

--- server/public/js/cart.js
+++ server/public/js/cart.js
@@ -1,41 +1,126 @@
 // Div внутри корзины, в который мы добавляем товары
 const cartWrapper =  document.querySelector('.cart-wrapper');
 
-// Отслеживаем клик на странице
-window.addEventListener('click', function (event) {
-    // Проверяем что клик был совершен по кнопке "Добавить в корзину"
-    if (event.target.hasAttribute('data-cart')) {
-        // Находим карточку с товаром, внутри котрой был совершен клик
-        const card = event.target.closest('.card');
+document.addEventListener('DOMContentLoaded', () => {
+    const cartProductsList = document.querySelector('.cart-wrapper');
+    const loc = window.location.pathname;
+    const dir = loc.split("/").pop();
 
-        // Собираем данные с этого товара и записываем их в единый объект productInfo
-        const productInfo = {
-            id: card.dataset.id,
-            imgUrl: card.querySelector('.product-img').getAttribute('src'),
-            name: card.querySelector('.item-title').innerText,
-            price: card.querySelector('.price__currency').innerText,
-            counter: card.querySelector('[data-counter]').innerText,
-        };
+    window.addEventListener('click', function (event) {
+
+        if(event.target.dataset.action === 'pizza') {
+            const size = event.target;
+            const card = event.target.closest('.card');
+            const sizeWrapper = event.target.closest('.size-wrapper');
+            const itemsControl = sizeWrapper.querySelectorAll('.items__control');
+
+            for(let i = 0; i<itemsControl.length; i++) {
+                itemsControl[i].classList.remove('active');
+            }
+            event.target.classList.add('active');
+            card.querySelector('.price__currency').innerText = `${size.getAttribute('data-sb-curent-price')} ₽`
+        }
 
 
-        // Проверять если ли уже такой товар в корзине
-        const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`);
+        // Объявляем переменную для счетчика
+        let counter;
 
-        // Если товар есть в корзине
-        if (itemInCart) {
-            const counterElement = itemInCart.querySelector('[data-counter]');
-            counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
-        } else {
-            // Если товара нет в корзине
+        // Проверяем клик строго по кнопкам Плюс либо Минус
+        if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+            // Находим обертку счетчика
+            const counterWrapper = event.target.closest('.counter-wrapper');
+            // Находим див с числом счетчика
+            counter = counterWrapper.querySelector('[data-counter]');
+        }
 
-            // Собранные данные подставим в шаблон для товара в корзине
-            const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
+        // Проверяем является ли элемент по которому был совершен клик кнопкой Плюс
+        if (event.target.dataset.action === 'plus') {
+            counter.innerText = ++counter.innerText;
+        }
+
+        // Проверяем является ли элемент по которому был совершен клик кнопкой Минус
+        if (event.target.dataset.action === 'minus') {
+
+            // Проверяем чтобы счетчик был больше 1
+            if (parseInt(counter.innerText) > 1) {
+                // Изменяем текст в счетчике уменьшая его на 1
+                counter.innerText = --counter.innerText;
+            } else if (event.target.closest('.cart-wrapper') && parseInt(counter.innerText) === 1) {
+                // Проверка на товар который находится в корзине
+                // Удаляем товар из корзины
+                event.target.closest('.cart-item').remove();
+
+                // Отображение статуса корзины Пустая / Полная
+                toggleCartStatus();
+
+                // Пересчет общей стоимости товаров в корзине
+                calcCartPriceAndDelivery();
+
+            }
+
+        }
+        updateStorage();
+        // Проверяем клик на + или - внутри коризины
+        if (event.target.hasAttribute('data-action') && event.target.closest('.cart-wrapper')) {
+            // Пересчет общей стоимости товаров в корзине
+            calcCartPriceAndDelivery();
+        }
+
+        // Проверяем что клик был совершен по кнопке "Добавить в корзину"
+        if (event.target.hasAttribute('data-cart')) {
+            // Находим карточку с товаром, внутри котрой был совершен клик
+            const card = event.target.closest('.card');
+            let productInfo;
+            // Собираем данные с этого товара и записываем их в единый объект productInfo
+            if(dir !== 'pizza') {
+                productInfo = {
+
+                    id: card.dataset.id,
+                    imgUrl: card.querySelector('.product-img').getAttribute('src'),
+                    name: card.querySelector('.item-title').innerText,
+                    price: card.querySelector('.price__currency').innerText,
+                    counter: card.querySelector('[data-counter]').innerText,
+                };
+            } else {
+                productInfo = {
+                    id: card.dataset.id,
+                    imgUrl: card.querySelector('.product-img').getAttribute('src'),
+                    name: card.querySelector('.item-title').innerText,
+                    price: card.querySelector('.price__currency').innerText,
+                    counter: card.querySelector('[data-counter]').innerText,
+                    size: card.querySelector('.active').innerText,
+                };
+            }
+
+
+            // Проверять если ли уже такой товар в корзине
+            const itemInCart = document.querySelector('.cart-wrapper').querySelector(`[data-size="${productInfo.size}"][data-id="${productInfo.id}"]`);
+
+
+            // Если товар есть в корзине
+            if (itemInCart) {
+                const counterElement = itemInCart.querySelector('[data-counter]');
+                counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+                updateStorage();
+            }
+            else {
+                // Если товара нет в корзине
+                let size;
+                if(dir==='pizza') {
+                    size = `<div class="cart-item__details">${productInfo.size} <span>см</span></div>`;
+                }
+                else {
+                    size = '';
+                }
+                // Собранные данные подставим в шаблон для товара в корзине
+                const cartItemHTML = `<div class="cart-item" data-size="${productInfo.size}" data-id="${productInfo.id}">
 								<div class="cart-item__top">
 									<div class="cart-item__img">
 										<img src="${productInfo.imgUrl}" alt="${productInfo.title}">
 									</div>
 									<div class="cart-item__desc">
 										<div class="cart-item__title">${productInfo.name}</div>
+										${size}
 
 										<!-- cart-item__details -->
 										<div class="cart-item__details">
@@ -57,21 +142,103 @@ window.addEventListener('click', function (event) {
 								</div>
 							</div>`;
 
-            // Отобразим товар в корзине
-            cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+                // Отобразим товар в корзине
+                cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+                updateStorage();
+            }
+
+            // Сбрасываем счетчик добавленного товара на "1"
+            card.querySelector('[data-counter]').innerText = '1';
+
+            // Отображение статуса корзины Пустая / Полная
+            toggleCartStatus();
+
+            // Пересчет общей стоимости товаров в корзине
+            calcCartPriceAndDelivery();
+
+        }
+        if(event.target.hasAttribute('data-toProcess')) {
+            localStorage.removeItem('cart-list');
+            location.reload();
         }
 
-        // Сбрасываем счетчик добавленного товара на "1"
-        card.querySelector('[data-counter]').innerText = '1';
 
-        // Отображение статуса корзины Пустая / Полная
-        toggleCartStatus();
+    });
 
-        // Пересчет общей стоимости товаров в корзине
-        calcCartPriceAndDelivery();
+    const initialState = () => {
+        if (localStorage.getItem('cart-list') !== null) {
+            cartProductsList.innerHTML = localStorage.getItem('cart-list');
+            toggleCartStatus();
+            calcCartPriceAndDelivery();
 
-    }
+
+            document.querySelectorAll('.cart-content__product').forEach(el => {
+                let id = el.dataset.id;
+                console.log(id)
+                document.querySelector(`.product[data-id="${id}"]`).querySelector('.product__btn').disabled = true;
+            });
+        }
+    };
+
+    initialState();
+
+    const updateStorage = () => {
+        let html = cartProductsList.innerHTML;
+        html = html.trim();
+
+        if (html.length) {
+            localStorage.setItem('cart-list', html);
+        }
+        else {
+            localStorage.removeItem('cart-list');
+        }
+
+    };
 });
+
+function calcCartPriceAndDelivery() {
+    const cartWrapper = document.querySelector('.cart-wrapper');
+    const priceElements = cartWrapper.querySelectorAll('.price__currency');
+    const totalPriceEl = document.querySelector('.total-price');
+    const deliveryCost = document.querySelector('.delivery-cost');
+    const cartDelivery = document.querySelector('[data-cart-delivery]');
+
+    // Общая стоимость товаров
+    let priceTotal = 0;
+
+    // Обходим все блоки с ценами в корзине
+    priceElements.forEach(function (item) {
+        // Находим количество товара
+        const amountEl = item.closest('.cart-item').querySelector('[data-counter]');
+        // Добавляем стоимость товара в общую стоимость (кол-во * цену)
+        priceTotal += parseInt(item.innerText) * parseInt(amountEl.innerText);
+    });
+    priceTotal += parseInt(deliveryCost.innerText);
+    // Отображаем цену на странице
+    totalPriceEl.innerText = priceTotal;
+
+    // Скрываем / Показываем блок со стоимостью доставки
+    if (priceTotal > 0) {
+        cartDelivery.classList.remove('none');
+    } else {
+        cartDelivery.classList.add('none');
+    }
+
+    // Указываем стоимость доставки
+    /*if (priceTotal >= 600) {
+        deliveryCost.classList.add('free');
+        deliveryCost.innerText = 'бесплатно';
+    } else {
+        deliveryCost.classList.remove('free');*/
+    deliveryCost.innerText = '250 ₽';
+    /*}
+
+     */
+}
+
+
+// Отслеживаем клик на странице
+
 
 
 /*
--- server/public/js/data.js
+++ server/public/js/data.js
@@ -2,31 +2,63 @@
 const url = "../json/thing.json";
 
 $.getJSON(url, function (data) {
-    console.log(data);
+    const loc = window.location.pathname;
+    const dir = loc.split("/").pop();
 
     for(let i = 0; i<data.length;i++) {
-        $('#products-container').append('<div class="col-md-6">\n' +
-            '                    <div class="card mb-4" data-id="'+data[i].id+'">\n' +
-            '                        <img class="product-img" src=".'+data[i].imgUrl+'" alt="">\n' +
-            '                        <div class="card-body text-center">\n' +
-            '                            <h4 class="item-title">'+data[i].name+'</h4>\n' +
-            '                            <div class="details-wrapper">\n' +
-            '                                <div class="items counter-wrapper">\n' +
-            '                                    <div class="items__control" data-action="minus">-</div>\n' +
-            '                                    <div class="items__current" data-counter>1</div>\n' +
-            '                                    <div class="items__control" data-action="plus">+</div>\n' +
-            '                                </div>\n' +
-            '\n' +
-            '                                <div class="price">\n' +
-            '                                    <div class="price__currency">'+data[i].price+' ₽</div>\n' +
-            '                                </div>\n' +
-            '                            </div>\n' +
-            '\n' +
-            '                            <button data-cart type="button" class="btn btn-block btn-outline-warning">+ в корзину</button>\n' +
-            '\n' +
-            '                        </div>\n' +
-            '                    </div>\n' +
-            '                </div>')
+        if(dir != 'pizza') {
+            $('#products-container').append('<div class="col-md-6">\n' +
+                '                    <div class="card mb-4" data-id="'+dir+ ' ' + data[i].id + '">\n' +
+                '                        <img class="product-img" src=".' + data[i].imgUrl + '" alt="">\n' +
+                '                        <div class="card-body text-center">\n' +
+                '                            <h4 class="item-title">' + data[i].name + '</h4>\n' +
+                '                            <div class="details-wrapper">\n' +
+                '                                <div class="items counter-wrapper">\n' +
+                '                                    <div class="items__control" data-action="minus">-</div>\n' +
+                '                                    <div class="items__current" data-counter>1</div>\n' +
+                '                                    <div class="items__control" data-action="plus">+</div>\n' +
+                '                                </div>\n' +
+                '\n' +
+                '                                <div class="price">\n' +
+                '                                    <div class="price__currency">' + data[i].price + ' ₽</div>\n' +
+                '                                </div>\n' +
+                '                            </div>\n' +
+                '\n' +
+                '                            <button data-cart type="button" class="btn btn-block btn-outline-warning">+ в корзину</button>\n' +
+                '\n' +
+                '                        </div>\n' +
+                '                    </div>\n' +
+                '                </div>')
+        }
+        else {
+            $('#products-container').append('<div class="col-md-6">\n' +
+                '                    <div class="card mb-4" data-id="'+dir+ ' ' + data[i].id + '">\n' +
+                '                        <img class="product-img" src=".'+data[i].imgUrl+'" alt="">\n' +
+                '                        <div class="card-body text-center">\n' +
+                '                            <h4 class="item-title">'+data[i].name+'</h4>\n' +
+                '                            <div class="details-wrapper">\n' +
+                '                                <div class="items counter-wrapper">\n' +
+                '                                    <div class="items__control" data-action="minus">-</div>\n' +
+                '                                    <div class="items__current" data-counter>1</div>\n' +
+                '                                    <div class="items__control" data-action="plus">+</div>\n' +
+                '                                </div>\n' +
+                                                '<div class="items size-wrapper">\n' +
+                                                    '<div class="items__control active" data-action="pizza" data-sb-curent-price="'+data[i].price30+'" data-sb-curent-size="30" data-sb-curent-id-or-vendor-code="0030pz">30\n</div>' +
+                                                    '<div class="items__control" data-action="pizza" data-sb-curent-price="'+data[i].price36+'" data-sb-curent-size="36" data-sb-curent-id-or-vendor-code="0036pz">36\n</div>' +
+                                                    '<div class="items__control" data-action="pizza" data-sb-curent-price="'+data[i].price50+'" data-sb-curent-size="50" data-sb-curent-id-or-vendor-code="0050pz">50\n</div>' +
+                                                '</div>\n' +
+
+                '                                <div class="price">\n' +
+                '                                    <div class="price__currency">'+data[i].price30+' ₽</div>\n' +
+                '                                </div>\n' +
+                '                            </div>\n' +
+                '\n' +
+                '                            <button data-cart type="button" class="btn btn-block btn-outline-warning">+ в корзину</button>\n' +
+                '\n' +
+                '                        </div>\n' +
+                '                    </div>\n' +
+                '                </div>')
+        }
     }
 });
 
